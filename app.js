d90cbffb2048d729a411574aaaeb8a076c964f95
“Thalles <talles.th54@gmail.com>
2023-08-26 15:11:48 -0300

    up.teste.0.0.1

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
 var valor1 = 33;
 var valor2 = 99;
 
+var valor3 = 10;
+var valor4 = 911;
 
-console.log('Valor total é: ', valor1 + valor2);
+
+console.log('A soma do valor 1 e 2: ', valor1 + valor2);
+
+console.log('A soma do valor 3 e 4: ', valor3 + valor4);
