2147bbfeaa945ee48ed7bc08f4dec22b2a7af0f6
“Thalles <talles.th54@gmail.com>
2023-08-28 21:54:56 -0300

    Eslint e o Prettier

--- app.js
+++ app.js
@@ -1,27 +1,25 @@
 // Importar Express
-import express from "express";
+import express from 'express';
 
 //App
 const app = express();
 
-
-
 // Rotas
 
-app.get("/", function (req, res) {
-  res.send("Birulinha 123 ");
+app.get('/', function (req, res) {
+  res.send('Birulinha 123 ');
 });
 
 //Endpoint's
 
-app.get("/user/:nome/sobrenome/:sobrenome", function (req, res) {
+app.get('/user/:nome/sobrenome/:sobrenome', function (req, res) {
   res.send({
     nome: req.params.nome,
-    sobrenome: req.params.sobrenome
+    sobrenome: req.params.sobrenome,
   });
 });
 
 // Servidor
 app.listen(8080, function () {
-  console.log("Servidor rodando !");
+  console.log('Servidor rodando !');
 });
