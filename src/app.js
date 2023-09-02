2a16db7006a9af16162fd2dd440f972d026e9939
“Thalles <talles.th54@gmail.com>
2023-09-01 22:15:28 -0300

    padrão MVC

--- src/app.js
+++ src/app.js
@@ -1,17 +1,14 @@
 // Importações
 import express from 'express';
-/*
-import { userRouter } from './modules/user/user.controller.js';
-import { productRouter } from './modules/products/product.controller.js';
-*/
-
 import { routerLoader } from './routerLoader.js';
 
 //App
 const app = express();
 
-// Rotas
+// Conversão para Json
+app.use(express.json());
 
+// Rotas
 routerLoader(app);
 
 // Servidor
