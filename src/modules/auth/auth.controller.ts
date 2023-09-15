6d2401beae26865fc1ceee40535e466a250d0c98
“Thalles <talles.th54@gmail.com>
2023-09-15 00:21:50 -0300

    AuthDTO

---
+++ src/modules/auth/auth.controller.ts
@@ -0,0 +1,24 @@
+import { Request, Response, Router } from 'express';
+import { AuthDTO } from './dtos/auth-insert.dto';
+import { validadeAuth } from './auth.service';
+import { ReturnError } from '@exceptions/dtos/return-error.dto';
+
+const authRouter = Router();
+
+const router = Router();
+
+authRouter.use('/auth', router);
+
+// METODO POST - criar usuario
+router.post(
+  '/',
+  async (req: Request<undefined, undefined, AuthDTO>, res: Response): Promise<void> => {
+    const user = await validadeAuth(req.body).catch((error) => {
+      new ReturnError(res, error);
+    });
+
+    res.send(user);
+  },
+);
+
+export default authRouter;
